8d4832e3a34efa500c1144a48bdb3e2cf1985d0f
Yoaquim Cintrón <yoaquim@outlook.com>
2023-08-23 19:29:05 -0400

    Add ability to download print resume

--- src/components/PrintBanner.tsx
+++ src/components/PrintBanner.tsx
@@ -1,12 +1,13 @@
-import React, {useContext} from 'react'
-import ThemeContext, {Theme} from '../context'
+import React, { useContext } from 'react'
+import ThemeContext, { Theme } from '../context'
+// @ts-ignore
+import pdf from '../docs/yoaquim-cintron.pdf'
 
 export default function PrintBanner() {
     const Theme: Theme = useContext(ThemeContext)
     return (
-        <div id="PrintBanner" className={`cursor-pointer py-1 w-screen fixed bottom-0 bg-gradient-to-r ${Theme.gradient} text-white text-center uppercase font-bold`}
-             onClick={window.print}>
-            [BETA] PRINT THIS RESUME
-        </div>
+        <a href={pdf} target="_blank" rel="noreferrer" className={`cursor-pointer py-1 w-screen fixed bottom-0 bg-gradient-to-r ${Theme.gradient} text-white text-center uppercase font-bold`}>
+            DOWNLOAD PRINT RESUME
+        </a>
     )
 }
