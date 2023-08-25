710a616b73e3e1b7873f790b66697796699ff33e
Yoaquim Cintrón <yoaquim@outlook.com>
2023-08-25 16:38:51 -0400

    install correct version of mixpanel, mixpanel-browser

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -6,10 +6,11 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import WhatImGoodAt from './components/WhatImGoodAt'
 import PrintBanner from './components/PrintBanner'
-import mixpanel from 'mixpanel'
+import mixpanel from 'mixpanel-browser'
 import ThemeContext, { Themes } from './context'
 
 function Resume() {
+
     mixpanel.init('967772639ff06d979a760099b3066fe5', {debug: false, track_pageview: true, persistence: 'localStorage'})
 
     const Resume = <div className={`lg:pb-24 pb-20 pt-8 grid place-items-center ${Themes.light.background} text-gray-600`}>
